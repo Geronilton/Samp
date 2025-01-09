86123cd8536565582cfca32509579c1dab727789
geronilton <geronilton001@gmail.com>
2025-01-09 00:41:26 -0300

    forms de assinantes, usuarios empresa e mensalidades

--- core/forms.py
+++ core/forms.py
@@ -1,9 +1,26 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import Empresas
+from .models import Empresas, EmpresaUser, Assinantes, Mensalidades
 
-class EmpresasForm(UserCreationForm):
+class EmpresaUserForm(UserCreationForm):
+    email = forms.EmailField(max_length=100, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = EmpresaUser
+        fields = ['username', 'email', 'password1', 'password2']
+
+class EmpresasForm(forms.ModelForm):
     class Meta:
         model = Empresas
-        fields = ['nome', 'email','password1', 'password2']
+        fields = ['nome', 'email']
+
+class AssinantesForm(forms.ModelForm):
+    class Meta:
+        model = Assinantes
+        fields = ['nome', 'email', 'telefone']
+
+class MensalidadesForm(forms.ModelForm):
+    class Meta:
+        model = Mensalidades
+        fields = ['valor', 'vencimento', 'status', 'data_pagamento']
 
