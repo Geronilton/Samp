7a1b5b8bbc385d09be1660bf85b0da3ab971838b
geronilton <geronilton001@gmail.com>
2025-01-09 00:42:30 -0300

    urls login, cruds de mensalidade e assinantes

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
-from .views import empresa_cadastro, empresas
+from .views import deletar_mensalidade,atualizar_mensalidade,mensalidades,cadastra_mensalidade,deletar_assinante, atualizar_assinante,assinantes,cadastro_assinantes, empresa_cadastro, empresas, empresa_user, home, login_view
 
 urlpatterns = [
     path('',empresa_cadastro, name='empresa_cadastro'),
+    path('home/',home, name='home'),
+    path('login/',login_view, name='login'),
     path('empresas/',empresas, name='empresas'),
+    path('empresa_user/<int:id>/',empresa_user, name='empresa_user'),
+    # ------ CRUD ASSINANTES---------
+    path('cadastro_assinantes/',cadastro_assinantes, name='cadastro_assinantes'),
+    path('assinantes/',assinantes, name='assinantes'),
+    path('atualizar_assinante/<int:id>/',atualizar_assinante, name='atualizar_assinante'),
+    path('deletar_assinante/<int:id>/',deletar_assinante, name='deletar_assinante'),
+    # -------------------------------
+    path('cadastra_mensalidade/<int:id>',cadastra_mensalidade, name='cadastra_mensalidade'),
+    path('mensalidades/',mensalidades, name='mensalidades'),
+    path('atualizar_mensalidade/<int:id>',atualizar_mensalidade, name='atualizar_mensalidade'),
+    path('deletar_mensalidade/<int:id>',deletar_mensalidade, name='deletar_mensalidade'),
 
 ]
 
