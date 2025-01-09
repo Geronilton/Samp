d1a1f7640a7b52000672734817f9e387ff8691c2
geronilton <geronilton001@gmail.com>
2025-01-09 00:41:42 -0300

    modelos atualizados

--- core/models.py
+++ core/models.py
@@ -1,26 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-class Empresas(AbstractUser):
-    nome = models.CharField('Nome',max_length=100)
-    email = models.EmailField('Email',max_length=100)
-
-    groups = models.ManyToManyField(
-        Group,
-        related_name="empresas_groups",  
-        blank=True,
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name="empresas_permissions",  
-        blank=True,
-    )
-
-    USERNAME_FIELD = 'email' 
+class Empresas(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    email = models.EmailField('Email', max_length=100, unique=True)
 
     def __str__(self):
         return self.nome
+
+class EmpresaUser(AbstractUser):
+    email = models.EmailField('Email', unique=True)  
+    empresa = models.ForeignKey(Empresas, on_delete=models.CASCADE)
+
+    USERNAME_FIELD = 'email'  
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email} - {self.empresa.nome}'
 
 class Assinantes(models.Model):
     nome = models.CharField('Nome',max_length=100)
