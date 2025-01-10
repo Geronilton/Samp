05a7f3f3e2a62433398d99458e2e57aa6b26d830
geronilton <geronilton001@gmail.com>
2025-01-09 22:30:57 -0300

    alterei inputs do formulario de mensalidade

--- core/forms.py
+++ core/forms.py
@@ -9,18 +9,38 @@ class EmpresaUserForm(UserCreationForm):
         model = EmpresaUser
         fields = ['username', 'email', 'password1', 'password2']
 
+
 class EmpresasForm(forms.ModelForm):
     class Meta:
         model = Empresas
         fields = ['nome', 'email']
+
 
 class AssinantesForm(forms.ModelForm):
     class Meta:
         model = Assinantes
         fields = ['nome', 'email', 'telefone']
 
+
 class MensalidadesForm(forms.ModelForm):
+
+    valor = forms.DecimalField(
+        max_digits=10,  # Número total de dígitos, incluindo os decimais
+        decimal_places=2,  # Número de casas decimais
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Digite o valor (somente números)',
+            'type': 'number',  # Garante entrada numérica no frontend
+            'step': '0.01',  # Permite valores decimais no campo
+        }),
+    )
     class Meta:
         model = Mensalidades
         fields = ['valor', 'vencimento', 'status', 'data_pagamento']
+        widgets = {
+            'status': forms.Select(attrs={'class': 'form-control'}),
+            'valor': forms.TextInput(attrs={'class': 'form-control'}),
+            'vencimento': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'data_pagamento': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+        }
 
